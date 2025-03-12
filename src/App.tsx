11ac9b7f71c46f8bb91505ba1b6b4e78a4db1b27
GianRaHu <96022843+GianRaHu@users.noreply.github.com>
2025-03-13 00:08:09 +0100

    Update App for game state saving.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from '@/components/ui/toaster';
-import Home from '@/pages/Home';
-import Game from '@/pages/Game';
-import DailyChallenge from '@/pages/DailyChallenge';
-import Stats from '@/pages/Stats';
-import History from '@/pages/History';
+import Home from './pages/Home';
+import Game from './pages/Game';
 
-export function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/play/:difficulty" element={<Game />} />
-        <Route path="/daily" element={<DailyChallenge />} />
-        <Route path="/stats" element={<Stats />} />
-        <Route path="/history" element={<History />} />
+        <Route path="/game/:difficulty" element={<Game />} />
       </Routes>
-      <Toaster />
     </Router>
   );
-}
+};
+
+export default App;
