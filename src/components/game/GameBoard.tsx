063fd177cf773e40b9f1a7a5cdc8fa95ac21bc02
GianRaHu <96022843+GianRaHu@users.noreply.github.com>
2025-03-13 15:47:53 +0100

    Create GameBoard.tsx

---
+++ src/components/game/GameBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './GameBoard.css';
+
+export const GameBoard: React.FC = () => {
+  return (
+    <div className="game-board">
+      <h1>Hashi Puzzle</h1>
+      <p>Game board implementation coming soon...</p>
+    </div>
+  );
+};
