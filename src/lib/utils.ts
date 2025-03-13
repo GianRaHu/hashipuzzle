3339671c5e9f0f5e34a0f2ed57403702ed4bbddd
GianRaHu <96022843+GianRaHu@users.noreply.github.com>
2025-03-13 10:11:38 +0100

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,6 @@
-
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { format, formatDistanceToNow } from "date-fns";
-
+ 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function formatTime(milliseconds: number): string {
-  const totalSeconds = Math.floor(milliseconds / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
-  
-  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-}
-
-export function formatTimeAgo(date: Date): string {
-  return formatDistanceToNow(date, { addSuffix: true });
 }
