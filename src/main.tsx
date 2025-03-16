30e7524a988219013cfcac84d9480bd5b5ad771b
GianRaHu <96022843+GianRaHu@users.noreply.github.com>
2025-03-16 20:17:13 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { initializeApp } from './utils/initApp';
 import './index.css';
-import './App.css';
 
-async function startApp() {
-  await initializeApp({
-    enableServiceWorker: true,
-    enableLovable: true
-  });
-
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
-startApp().catch(console.error);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
